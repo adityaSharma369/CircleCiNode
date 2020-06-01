f5b1c1e2286bb660f04fc9e70848529ced4c57d1
adityasharma369 <adityadamera369@gmail.com>
2020-06-01 10:49:39 +0530

    these are some demo changes

--- sketch.js
+++ sketch.js
@@ -15,14 +15,14 @@ function prod(a, b) {
 }
 
 function answer() {
-  return 42;
+  return 44;
 }
 
 function digital_root(n) {
   return (n-1)%9+1;
 }
-function sum42(a, b) {
-  return a + b + 42;
+function sum44(a, b) {
+  return a + b + 44;
 }
 
 function anomalyCode(x) {
@@ -33,7 +33,7 @@ module.exports = {
   sum: sum,
   sub: sub,
   prod: prod,
-  sum42: sum42,
+  sum44: sum44,
   digital_root: digital_root,
   sayHelloTo: sayHelloTo,
   answer: answer,
